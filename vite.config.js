3f5c357a121ba99a778c71b01915e0eaf20f05db
YangTao0326 <tyan0076@student.monash.edu>
2025-08-13 15:37:04 +0800

    fix vite base config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -14,5 +13,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? '/FIT5032_library/'  // GitHub Pages 仓库名
+    : '/',
 })
